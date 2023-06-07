4a6b78ebcc8bb1de7247665c7618dd476c0cec4b
Christian Axtmann <christian.axtmann@posteo.de>
2023-06-07 23:34:14 +0200

    finished

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
+  @Input() darkMode = true;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
 }
